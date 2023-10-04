10bd2c6ac7a364985aa96a241abb433c292a5409
Daniel Magro <dando385@yahoo.com>
2023-10-04 16:03:10 -0400

    1st half of video 2 complete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import { useState } from 'react'; // import useState
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   function addTodo(text) {
-    setTodos([...todos, text])
+    setTodos([...todos, text]);
   }
 
   function removeTodo(index) {
-    // Checks whether the index matches the index of the item we want to remove
-    // If it does, filter it out of the array
-
-   setTodos(todos.filter((todo, i) => i !== index))
-
+    setTodos(todos.filter((todo, i) => i !== index));
   }
+
   return (
-  <div className="App">
-    <h1>Todo List</h1>
-    <form>
-      
-    </form>
-      <input type="text" />
-      <button type='submit' >Add Todo</button>
-  </div>
+    <div className="App">
+      <h1>Todo List</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addTodo(event.target.elements.todo.value);
+          event.target.elements.todo.value = '';
+        }}
+      >
+        {/* Input field for adding todos */}
+        <input type='text' name='todo' />
+        <button type='submit'>Add Todo</button>
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
-
 export default App;
+
