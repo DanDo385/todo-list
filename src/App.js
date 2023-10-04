8bfda91fd64fc61de3481821b7f71c6892846b82
Daniel Magro <dando385@yahoo.com>
2023-10-04 15:11:52 -0400

    add form, text, submit button to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([])
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+    <h1>Todo List</h1>
+    <form>
+      
+    </form>
+      <input type="text" />
+      <button type='submit' >Add Todo</button>
+  </div>
+  );
 }
+
 
 export default App;
