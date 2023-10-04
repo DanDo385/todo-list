a1dcc2f37cad4a17435f2e6c06409e395d88814b
Daniel Magro <dando385@yahoo.com>
2023-10-04 15:16:11 -0400

    1st video of etb todo project complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([])
+
+  function addTodo(text) {
+    setTodos([...todos, text])
+  }
+
+  function removeTodo(index) {
+    // Checks whether the index matches the index of the item we want to remove
+    // If it does, filter it out of the array
+
+   setTodos(todos.filter((todo, i) => i !== index))
+
+  }
   return (
   <div className="App">
     <h1>Todo List</h1>
